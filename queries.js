0125cc952b7c8e9613fecf0851ed3ee969bbe3d3
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-31 21:23:15 +0000

    Renamed variables and changed to check for first/last name and return all matching people instead of one.

--- queries.js
+++ queries.js
@@ -1,11 +1,13 @@
 const db = require('./db');
 
 module.exports = {
-  getPersonBySurname: (surname, done) => {
+  getPeopleByName: (name, done) => {
     db.connect((error, client) => {
-      const query = "SELECT * FROM famous_people WHERE last_name = $1::text;";
-      client.query(query, [surname], (err, result) => {
-        done(result.rows[0]);
+      console.log("Searching...");
+
+      const query = "SELECT * FROM famous_people WHERE last_name = $1::text OR first_name = $1::text;";
+      client.query(query, [name], (err, result) => {
+        done(result.rows);
         db.close(client);
       });
     });
