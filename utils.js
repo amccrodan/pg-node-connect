eedab63bff603ad70d6db94501edc315bb6234d6
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-31 21:28:04 +0000

    Added inline person. reference in loop instead of creating 4 variables.

--- utils.js
+++ utils.js
@@ -6,12 +6,7 @@ module.exports = {
     console.log(`Found ${people.length} person(s) matching your search.`);
 
     for (let person of people) {
-      const id = person.id;
-      const first_name = person.first_name;
-      const last_name = person.last_name;
-      const birthdate = person.birthdate;
-
-      console.log(`- ${id}: ${first_name} ${last_name}, born '${birthdate}'`);
+      console.log(`- ${person.id}: ${person.first_name} ${person.last_name}, born '${person.birthdate}'`);
     }
   }
 }
