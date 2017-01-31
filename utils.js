07a88de4636eb6a61aeb13e906909c357c0d502b
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-31 21:24:02 +0000

    Changed Output function to print results and is now able to handle an array of found people.

--- utils.js
+++ utils.js
@@ -2,12 +2,16 @@ module.exports = {
   getInput : () => {
     return process.argv[2];
   },
-  formatOutput : (person) => {
-    const id = person.id;
-    const first_name = person.first_name;
-    const last_name = person.last_name;
-    const birthdate = person.birthdate;
+  printOutput : (people) => {
+    console.log(`Found ${people.length} person(s) matching your search.`);
 
-    return `- ${id}: ${first_name} ${last_name}, born '${birthdate}'`;
+    for (let person of people) {
+      const id = person.id;
+      const first_name = person.first_name;
+      const last_name = person.last_name;
+      const birthdate = person.birthdate;
+
+      console.log(`- ${id}: ${first_name} ${last_name}, born '${birthdate}'`);
+    }
   }
 }
