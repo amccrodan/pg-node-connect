e3791e09b8377806780952222578832403c18052
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-01-31 21:21:33 +0000

    Renamed file to lookup-name.js

--- lookup-surname.js
+++
@@ -1,6 +0,0 @@
-const database = require('./queries');
-const utils = require('./utils');
-
-database.getPersonBySurname(utils.getInput(), (person) => {
-  console.log(utils.formatOutput(person));
-});
